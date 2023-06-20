2a788ca99ecdf195b3d96b1786349a7d00966d32
Betacoder12 <131929462+Betacoder12@users.noreply.github.com>
2023-06-20 09:32:25 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+function doSomething (num: number, num2: number, sprite: game.LedSprite) {
+    led.plot(num, num2)
+    if (_1 == "left") {
+        if (sprite.get(LedSpriteProperty.X) == num && sprite.get(LedSpriteProperty.Y) == num2) {
+        	
+        }
+    }
+}
+let _1 = ""
+_1 = ""
+basic.forever(function () {
+	
+})
